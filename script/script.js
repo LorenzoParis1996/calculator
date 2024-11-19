71b0c6b5067fb6be6c2cf15fd96af83eafe13b2d
LorezoParis1996 <lorenzoparis86@live.it>
2024-11-19 13:46:23 +0100

    added logic

---
+++ script/script.js
@@ -0,0 +1,71 @@
+const output = document.getElementById('output');
+//querySelectorAll supports the forEach method
+const numbers = document.querySelectorAll('.number');
+const operators = document.querySelectorAll('.operator');
+const equals = document.getElementById('equals');
+const clearInput = document.getElementById('clear');
+
+//values to be stored
+let numOne = null;
+let operator = null;
+
+numbers.forEach(number => {
+    number.addEventListener('click', () => {
+        output.textContent += number.textContent
+    });
+});
+
+//we check if there is a number displayed. If there is, we store the number as "numOne", then we store the selected operator, and it clears the input displayed so that a new number can be entered for calculation.
+
+operators.forEach(ope => {
+    ope.addEventListener('click', () =>{
+        if(output.textContent) {
+            numOne = parseFloat(output.textContent);
+            operator = ope.textContent;
+            output.textContent = '';
+        }
+    });
+});
+
+//if the first number is entered, an operator is selected, and a second number has been entered and is being displayed, we can proceed with the calculation.
+equals.addEventListener('click', () => {
+    if (numOne !== null && operator && output.textContent) {
+        const numTwo = parseFloat(output.textContent);
+        let result;
+        
+        switch(operator) {
+            case '+':
+                result = numOne + numTwo;
+                break;
+            
+            case '-':
+                result = numOne - numTwo;
+                break;
+
+            case 'x':
+                result = numOne * numTwo;
+                break;
+
+            case '/':
+                result = numOne / numTwo;
+                break;
+        }
+        
+        //displays the result
+        output.textContent = result;
+
+        //resets stored values
+        numOne = null;
+        operator = null;
+    }
+});
+
+//clears output and stored values
+clearInput.addEventListener('click', () => {
+    output.textContent = '';
+    numOne = null;
+    operator = null;
+});
+
+
+
